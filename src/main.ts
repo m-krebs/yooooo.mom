d0705219eb46abceffead5d261dd795796d90b75
Michael Krebs <michikrbs@gmail.com>
2023-05-29 00:34:06 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import "./style.css";
-import { setJoke } from "./joke.ts";
+import {
+  loadJokes,
+  getJokeByCategory,
+  setJoke,
+  getRandomJoke,
+} from "./joke.ts";
+import refreshLogo from "./refresh.svg";
+
+loadJokes();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    </div>
-    <div id="joke" class="text-3xl"></div>
+    <button id="refresh" class="bg-transparent border-none"><img class="invert w-5" src="${refreshLogo}" alt="refresh button"/></button>
   </div>
+  <div id="joke" class="text-3xl"></div>
 `;
 
-setJoke(document.querySelector<HTMLButtonElement>("#joke")!);
+setJoke(document.querySelector<HTMLElement>("#joke")!, getRandomJoke());
+
+document.querySelector("#refresh")!.addEventListener("click", () => {
+  refreshJoke();
+  const refreshButton: HTMLElement = document.querySelector("#refresh")!;
+  refreshButton.classList.add("animated");
+  setTimeout(() => {
+    refreshButton.classList.remove("animated");
+  }, 300);
+});
+
+function refreshJoke() {
+  setJoke(
+    document.querySelector<HTMLElement>("#joke")!,
+    getJokeByCategory("stupid")
+  );
+}
