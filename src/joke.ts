9085150e7780de0faef67439cb304a8348b27bea
Michael Krebs <michikrbs@gmail.com>
2023-05-29 00:27:50 +0200

    feat: add joke funtctionalities and localstorage impl.

--- src/joke.ts
+++ src/joke.ts
@@ -1,11 +1,36 @@
-export async function setJoke(element: HTMLElement) {
+export async function loadJokes() {
   await fetch("./jokes.json")
     .then((response) => response.json())
-    .then((json) =>
-      set(element, json["fat"][Math.floor(Math.random() * json["fat"].length)])
-    );
+    .then((json) => {
+      const jokesString = JSON.stringify(json);
+      localStorage.setItem("cachedJokes", jokesString);
+    });
 }
 
-function set(element: HTMLElement, joke: string) {
+function getCachedJokes() {
+  const cachedJokesString = localStorage.getItem("cachedJokes");
+  if (cachedJokesString === null) {
+    return null;
+  }
+  return JSON.parse(cachedJokesString);
+}
+
+export function getRandomJoke() {
+  const jokes = getCachedJokes();
+  let allJokes: any = [];
+  Object.keys(jokes).forEach((val) => {
+    jokes[val].forEach((joke: string) => {
+      allJokes.push(joke);
+    });
+  }, []);
+  return allJokes[Math.floor(Math.random() * allJokes.length)];
+}
+
+export function getJokeByCategory(category: string) {
+  const jokes = getCachedJokes();
+  return jokes[category][Math.floor(Math.random() * jokes[category].length)];
+}
+
+export function setJoke(element: HTMLElement, joke: string) {
   element.innerHTML = joke;
 }
